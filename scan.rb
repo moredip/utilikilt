6434035f4528335a0e7b0a2cdab1acd2be2f6c88
Pete Hodgson <git@thepete.net>
2012-03-27 23:56:20 -0700

    spiking bare ruby implementation or pipeline

---
+++ scan.rb
@@ -0,0 +1,35 @@
+require 'pathname'
+require 'fileutils'
+
+INPUT_BASE = Pathname.new( "source" ).expand_path
+OUTPUT_BASE = Pathname.new( "public" ).expand_path
+
+INPUT_TO_OUTPUT_EXT_MAP = {
+  'md' => 'html',
+  'markdown' => 'html',
+  'haml' => 'html',
+  'sass' => 'css',
+  'scss' => 'css',
+  'coffee' => 'js'
+}
+
+def output_for_input input
+  relative = input.relative_path_from INPUT_BASE
+  
+  # this is a bit fiddly because Pathname defines exts as including the leading dot while our map does not include it
+  output_ext = '.'+INPUT_TO_OUTPUT_EXT_MAP[input.extname[1..-1]]
+
+  raise ArgumentError, "unrecognized input file extension for #{input}" if output_ext.nil?
+
+  File.join( OUTPUT_BASE, relative.sub_ext(output_ext) )
+end
+
+input_file_glob = '*.{'+INPUT_TO_OUTPUT_EXT_MAP.keys.join(',')+'}'
+Pathname.glob(File.join( INPUT_BASE, "**", input_file_glob )).each do |input|
+  output = output_for_input(input)
+  if FileUtils.uptodate?( output, [input] ) 
+    puts "UP TO DATE: #{input} => #{output}"
+  else
+    puts "NEEDS UPDATE: #{input} => #{output}"
+  end
+end
